ab3a3f67697f1dc8d7497a6894ddf32776fcc2fe
Dimondwill <damianr6564@nycstudents.net>
2025-09-26 10:22:26 -0400

    Update dict_shop.py

--- dict_shop.py
+++ dict_shop.py
@@ -1,32 +1,87 @@
 cart = []
+price = []
 def main():
     while True:
-        dict = int(input("pick which dicionary you want, 1 1 or 1. Or do you want to proceed to checkout: "))
+        dict = int(input("pick which dicionary you want, 1 2 or 3. Or do you want to proceed to checkout: "))
 
         if dict == 1 or 2 or 3:
 
             if dict == 1:
-                print("this is the, animals dictonary it cost $15 it is 150 pages")
+                print("this is the, animals dictonary it cost $14.99 it is 150 pages")
                 opt = input("do you want it, yes or no: ")
                 opt = str(opt)
-                dict_1{
-                    "name": ""
-
-
+                dict_1 = {
+                    "name": "Animals dict.",
+                    "price": 14.99,
+                    "page count": 150
                 }
 
                 if opt == "yes":
-                    cart.append(dict_1)
-                    nxt = input("do you want to continue shopping , view cart, or proceed to checkout")
+                    cart.append(dict_1["name"])
+                    price.append(dict_1["price"])
+                    nxt = input("do you want to continue shopping , view cart, or proceed to checkout: ")
                     if nxt == "continue shopping":
                         continue
-                elif nxt == "view cart":
-                    print(f"this is your cart, dict{cart}")
-                    continue
+                    elif nxt == "view cart":
+                        print(f"this is your cart, dict{cart}, is costs {sum(price)}")
+                        continue
 
                 elif opt == "no":
                     continue
                     
                 else:
                     print("i dont understand your terminology, make sure youre not using caps")
+
+            if dict == 2:
+                print("this is the, cars dictonary it cost $19.99 it is 300 pages")
+                opt = input("do you want it, yes or no: ")
+                opt = str(opt)
+                dict_2 = {
+                    "name": "cars dict.",
+                    "price": 19.99,
+                    "page count": 300
+                }
+
+                if opt == "yes":
+                    cart.append(dict_2["name"])
+                    price.append(dict_2["price"])
+                    nxt = input("do you want to continue shopping , view cart, or proceed to checkout: ")
+                    if nxt == "continue shopping":
+                        continue
+                    elif nxt == "view cart":
+                        print(f"this is your cart, dict{cart}, is costs {sum(price)}")
+                        continue
+
+                elif opt == "no":
+                    continue
+                    
+                else:
+                    print("i dont understand your terminology, make sure youre not using caps")
+            if dict == 3:
+                print("this is the, Russian dictonary it cost $49.99 it is 980 pages")
+                opt = input("do you want it, yes or no: ")
+                opt = str(opt)
+                dict_3 = {
+                    "name": "russian dict.",
+                    "price": 49.99,
+                    "page count": 980
+                }
+
+                if opt == "yes":
+                    cart.append(dict_3["name"])
+                    price.append(dict_3["price"])
+                    nxt = input("do you want to continue shopping , view cart, or proceed to checkout: ")
+                    if nxt == "continue shopping":
+                        continue
+                    elif nxt == "view cart":
+                        print(f"this is your cart, dict{cart}, is costs {sum(price)}")
+                        continue
+
+                elif opt == "no":
+                    continue
+                    
+                else:
+                    print("i dont understand your terminology, make sure youre not using caps")
+        elif dict = "proceed to checkout":
+            print(f"")
 main()
